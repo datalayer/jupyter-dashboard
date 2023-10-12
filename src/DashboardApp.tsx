1cf7b4d121a59c15c192e28e946a5aace675edb9
Eric Charles <eric@datalayer.io>
2023-10-12 17:49:49 +0200

    chore: init

---
+++ src/DashboardApp.tsx
@@ -0,0 +1,17 @@
+/// <reference types="webpack-env" />
+
+import { createRoot } from 'react-dom/client';
+import DashboardJupyterLab from './DashboardJupyterLab';
+
+const div = document.createElement('div');
+document.body.appendChild(div);
+const root = createRoot(div)
+
+if (module.hot) {
+  module.hot.accept('./DashboardJupyterLab', () => {
+    const DashboardJupyterLab = require('./DashboardJupyterLab').default;
+    root.render(<DashboardJupyterLab/>);
+  })
+}
+
+root.render(<DashboardJupyterLab />);
